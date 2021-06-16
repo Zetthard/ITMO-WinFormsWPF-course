570d7092e3f147bc29e28d81ecb30133ba6e37b9
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-06-16 14:15:54 +0300

    fixing repo

---
+++ FirstSimpleForm/nForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace FirstSimpleForm
+{
+    partial class nForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CloseForm
+            // 
+            this.CloseForm.Location = new System.Drawing.Point(286, 267);
+            this.CloseForm.Name = "CloseForm";
+            this.CloseForm.Size = new System.Drawing.Size(242, 80);
+            this.CloseForm.TabIndex = 1;
+            this.CloseForm.Text = "Close";
+            this.CloseForm.UseVisualStyleBackColor = true;
+            this.CloseForm.Click += new System.EventHandler(this.CloseForm_Click);
+            // 
+            // nForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CloseForm);
+            this.Name = "nForm";
+            this.Text = "nForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CloseForm;
+    }
+}
