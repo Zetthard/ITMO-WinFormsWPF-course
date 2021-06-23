cfa2f4153edaad7e0322b1b3675b8dec1b17e2a5
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-06-23 10:53:45 +0300

    Ex1 done

---
+++ Lab02/WinQuestion/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WinQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // YesButton
+            // 
+            this.YesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.YesButton.Location = new System.Drawing.Point(53, 87);
+            this.YesButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(70, 45);
+            this.YesButton.TabIndex = 0;
+            this.YesButton.Text = "Да";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.Location = new System.Drawing.Point(213, 87);
+            this.NoButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(70, 45);
+            this.NoButton.TabIndex = 1;
+            this.NoButton.Text = "Нет";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NoButton_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вы довольны своей зарплатой?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 157);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Насущный вопрос";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab02/WinQuestion/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinQuestion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы и не сомневались!");
+        }
+
+        private void NoButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            NoButton.Top -= e.Y;
+            NoButton.Left += e.X;
+            if (NoButton.Top < -10 || NoButton.Top > 100)
+                NoButton.Top = 60;
+            if (NoButton.Left < -80 || NoButton.Left > 250)
+                NoButton.Left = 120;
+        }
+    }
+}
