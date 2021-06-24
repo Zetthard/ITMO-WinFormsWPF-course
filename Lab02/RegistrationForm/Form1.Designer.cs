23fa1b29a07514743eb066037978abe4652ba4ea
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-06-24 12:53:00 +0300

    Lab2 ex 8 (Registration form) done

---
+++ Lab02/RegistrationForm/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace RegistrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpBoxInput = new System.Windows.Forms.GroupBox();
+            this.labelPin = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxPin = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkBoxAdvanced = new System.Windows.Forms.CheckBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.grpBoxInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBoxInput
+            // 
+            this.grpBoxInput.Controls.Add(this.labelPin);
+            this.grpBoxInput.Controls.Add(this.labelName);
+            this.grpBoxInput.Controls.Add(this.textBoxPin);
+            this.grpBoxInput.Controls.Add(this.textBoxName);
+            this.grpBoxInput.Location = new System.Drawing.Point(28, 65);
+            this.grpBoxInput.Name = "grpBoxInput";
+            this.grpBoxInput.Size = new System.Drawing.Size(373, 145);
+            this.grpBoxInput.TabIndex = 0;
+            this.grpBoxInput.TabStop = false;
+            this.grpBoxInput.Text = "Введите регистрационные данные";
+            // 
+            // labelPin
+            // 
+            this.labelPin.AutoSize = true;
+            this.labelPin.Location = new System.Drawing.Point(50, 77);
+            this.labelPin.Name = "labelPin";
+            this.labelPin.Size = new System.Drawing.Size(25, 13);
+            this.labelPin.TabIndex = 3;
+            this.labelPin.Text = "PIN";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(47, 38);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Name";
+            // 
+            // textBoxPin
+            // 
+            this.textBoxPin.Location = new System.Drawing.Point(136, 73);
+            this.textBoxPin.Name = "textBoxPin";
+            this.textBoxPin.Size = new System.Drawing.Size(190, 20);
+            this.textBoxPin.TabIndex = 1;
+            this.textBoxPin.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxPin_Validating);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(136, 33);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(190, 20);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxName_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите тип регистрации";
+            // 
+            // chkBoxAdvanced
+            // 
+            this.chkBoxAdvanced.AutoSize = true;
+            this.chkBoxAdvanced.Location = new System.Drawing.Point(208, 219);
+            this.chkBoxAdvanced.Name = "chkBoxAdvanced";
+            this.chkBoxAdvanced.Size = new System.Drawing.Size(169, 17);
+            this.chkBoxAdvanced.TabIndex = 2;
+            this.chkBoxAdvanced.Text = "Расширенные возможности";
+            this.chkBoxAdvanced.UseVisualStyleBackColor = true;
+            this.chkBoxAdvanced.CheckedChanged += new System.EventHandler(this.chkBoxAdvanced_CheckedChanged);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(49, 261);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(127, 27);
+            this.buttonRegister.TabIndex = 3;
+            this.buttonRegister.Text = "Регистрация";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.chkBoxAdvanced);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grpBoxInput);
+            this.MaximumSize = new System.Drawing.Size(450, 350);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpBoxInput.ResumeLayout(false);
+            this.grpBoxInput.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxInput;
+        private System.Windows.Forms.Label labelPin;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxPin;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkBoxAdvanced;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Lab02/RegistrationForm/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chkBoxAdvanced_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBoxAdvanced.Checked == true)
+            {
+                Label lbl = new Label();
+                lbl.Location = new System.Drawing.Point(16, 96);
+                lbl.Size = new System.Drawing.Size(32, 23);
+                lbl.Name = "labelll";
+                lbl.TabIndex = 2;
+                lbl.Text = "PIN2";
+                grpBoxInput.Controls.Add(lbl);
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(96, 96);
+                txt.Size = new System.Drawing.Size(184, 20);
+                txt.Name = "textboxx";
+                txt.TabIndex = 1;
+                txt.Text = "";
+                grpBoxInput.Controls.Add(txt);
+                txt.KeyPress += new KeyPressEventHandler(this.textBoxName_KeyPress);
+
+            }
+            else 
+            {
+                int lcv;
+                lcv = grpBoxInput.Controls.Count;// определяется количество элементов
+                while (lcv > 4)
+                {
+                    grpBoxInput.Controls.RemoveAt(lcv - 1);
+                    lcv -= 1;
+                }
+
+            }
+        }
+
+        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле Name не может содежать цифры");
+                errorProvider1.SetError(textBoxName, "Must be letter");
+            }
+        }
+
+        private void textBoxPin_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBoxPin.Text == "")
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                try
+                {
+                    double.Parse(textBoxPin.Text);
+                    e.Cancel = false;
+                }
+                catch
+                {
+                    e.Cancel = true;
+                    MessageBox.Show("Поле PIN не может содержать буквы");
+                }
+            }
+
+            //private void textBoxPin_KeyPress(object sender, KeyPressEventArgs e)
+            //{
+            //     if (!char.IsDigit(e.KeyChar))
+            //    {
+            //        e.Handled = true;
+            //        MessageBox.Show("Поле PIN может содержать только цифры");
+            //    }
+            //}
+        }
+    }
+}
