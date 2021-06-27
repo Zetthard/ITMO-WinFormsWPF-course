d768fbe75e87e1fe90be2f2e3d3c1f6c0f86e1f6
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-06-27 20:34:27 +0300

    Lab 3.1 (UserControl elment) done

---
+++ Lab03/Lab03WinFormsControlLibrary/MyUserControlElement.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab03WinFormsControlLibrary
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 171);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(243, 219);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Lab03/WinTimer1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WinTimer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonRunPause = new System.Windows.Forms.Button();
+            this.userControl1 = new WinTimer1.UserControl1();
+            this.SuspendLayout();
+            // 
+            // ButtonRunPause
+            // 
+            this.ButtonRunPause.Location = new System.Drawing.Point(303, 297);
+            this.ButtonRunPause.Name = "ButtonRunPause";
+            this.ButtonRunPause.Size = new System.Drawing.Size(144, 68);
+            this.ButtonRunPause.TabIndex = 1;
+            this.ButtonRunPause.Text = "Run/Pause";
+            this.ButtonRunPause.UseVisualStyleBackColor = true;
+            this.ButtonRunPause.Click += new System.EventHandler(this.ButtonRunPause_Click);
+            // 
+            // userControl1
+            // 
+            this.userControl1.IsTimeEnabled = true;
+            this.userControl1.Location = new System.Drawing.Point(292, 94);
+            this.userControl1.Name = "userControl1";
+            this.userControl1.Size = new System.Drawing.Size(169, 144);
+            this.userControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonRunPause);
+            this.Controls.Add(this.userControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl1;
+        private System.Windows.Forms.Button ButtonRunPause;
+    }
+}
+
