7fe48e6bd013c47d1a32abefe43326179ab90321
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-05 13:35:10 +0300

    Lab 7 (1-4 out of 5) done

---
+++ Lab07/WinAsyncMethod/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WinAsyncMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.buttonWork = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonSubstract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Value A";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Value B";
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Location = new System.Drawing.Point(31, 162);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(155, 46);
+            this.buttonSum.TabIndex = 4;
+            this.buttonSum.Text = "Sum";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // buttonWork
+            // 
+            this.buttonWork.Location = new System.Drawing.Point(464, 162);
+            this.buttonWork.Name = "buttonWork";
+            this.buttonWork.Size = new System.Drawing.Size(155, 46);
+            this.buttonWork.TabIndex = 5;
+            this.buttonWork.Text = "Work";
+            this.buttonWork.UseVisualStyleBackColor = true;
+            this.buttonWork.Click += new System.EventHandler(this.buttonWork_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(372, 41);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(79, 25);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Result:";
+            // 
+            // buttonSubstract
+            // 
+            this.buttonSubstract.Location = new System.Drawing.Point(242, 162);
+            this.buttonSubstract.Name = "buttonSubstract";
+            this.buttonSubstract.Size = new System.Drawing.Size(155, 46);
+            this.buttonSubstract.TabIndex = 7;
+            this.buttonSubstract.Text = "Diff";
+            this.buttonSubstract.UseVisualStyleBackColor = true;
+            this.buttonSubstract.Click += new System.EventHandler(this.buttonSubstract_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 246);
+            this.Controls.Add(this.buttonSubstract);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonWork);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Async Run";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.Button buttonWork;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonSubstract;
+    }
+}
+
