dc776d82d31b4ee58bdd0e5bff64cdd81e738f0e
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-05 15:25:08 +0300

    Lab07 completed

---
+++ Lab07/WinAsyncEratosfen/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WinAsyncEratosfen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 263);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab07/WinAsyncEratosfen/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAsyncEratosfen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            try
+            {
+                a = int.Parse(textBox1.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Type conversion error");
+                textBox1.Text = "";
+                return;
+            }
+
+            string res = await Eratosfen(a);
+            label1.Text = res;
+        }
+
+        private async Task<string> Eratosfen(int a)
+        {
+            return await Task.Run(() =>
+            {
+                return GoButt(a);
+            });
+        }
+
+        public string GoButt(int maxValue)
+        {
+            StringBuilder resultText = new StringBuilder();
+            if (int.TryParse(textBox1.Text, out maxValue))
+            {
+                for (int trial = 2; trial <= maxValue; trial++)
+                {
+                    bool isPrime = true;
+                    for (int divisor = 2; divisor <= Math.Sqrt(trial); divisor++)
+                    {
+                        if (trial % divisor == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        resultText.AppendFormat("{0} ", trial);
+                    }
+                }
+            }
+            else
+            {
+                resultText.Append("Unable to parse maximum value.");
+            }
+            return resultText.ToString();
+        }
+    }
+}
